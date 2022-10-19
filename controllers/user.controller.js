b14ed18f2b39c29984e2201ef62e5af9beafd7f4
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-10-18 21:01:04 -0500

    se inserta el primer dato a la base de datos a mongo db

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,51 +1,55 @@
-const { request, response } = require('express')
+const { request, response } = require("express");
+const User = require("../models/user");
 
 const getUsers = (req = request, res = response) => {
   // url/api/users/?name=Sergio&date=2022-01-25  -> query
-  const { name, date } = req.query
+
+  const { name, date } = req.query;
 
   req.res.status(200).json({
-    msg: 'Get - controller',
+    msg: "Get - controller",
     name,
     date,
-  })
-}
+  });
+};
 
 const getUserById = (req = request, res = response) => {
   // url/api/users/25 -> Segmento: El 25 entra en el id
-  const id = req.params.id
+  const id = req.params.id;
   res.json({
-    msg: 'Usuario por id - controller',
+    msg: "Usuario por id - controller",
     id,
-  })
-}
+  });
+};
 
-const createUser = (req = request, res = response) => {
+const createUser = async (req = request, res = response) => {
   // url/api/users/ -> Body: Es el objeto en JSON
-  const { name, lastname, age } = req.body
+  const body = req.body;
+  const user = new User(body);
+  await user.save();
   res.status(201).json({
-    msg: 'post API - controller',
-    name,
-  })
-}
+    msg: "post API - controller",
+    user,
+  });
+};
 
 const updateUser = (req = request, res = response) => {
-  const id = req.params.id
-  const body = req.body
+  const id = req.params.id;
+  const body = req.body;
 
   res.json({
-    msg: 'put API - controller',
+    msg: "put API - controller",
     id,
     body,
-  })
-}
+  });
+};
 
 const deleteUser = (req = request, res = response) => {
-  const id = req.params.id
+  const id = req.params.id;
   res.json({
-    msg: 'delete API - Controller',
-  })
-}
+    msg: "delete API - Controller",
+  });
+};
 
 module.exports = {
   getUsers,
@@ -53,4 +57,4 @@ module.exports = {
   createUser,
   updateUser,
   deleteUser,
-}
+};
